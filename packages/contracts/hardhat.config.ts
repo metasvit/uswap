d17d05f8574919260adf310c580e82152faa7bf9
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2023-07-22 13:43:46 +0200

    add contracts skeleton

---
+++ packages/contracts/hardhat.config.ts
@@ -0,0 +1,51 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-solhint';
+import 'hardhat-tracer';
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+  },
+  networks: {
+    chiado: {
+      url: `https://rpc.chiadochain.net`,
+      chainId: 10200,
+      accounts: process.env.USWAP_PRIVATE_KEY
+        ? [process.env.USWAP_PRIVATE_KEY]
+        : undefined,
+      loggingEnabled: true,
+    },
+    lineaTestnet: {
+      url: `https://rpc.goerli.linea.build`,
+      chainId: 59140,
+      accounts: process.env.USWAP_PRIVATE_KEY
+        ? [process.env.USWAP_PRIVATE_KEY]
+        : undefined,
+      loggingEnabled: true,
+    },
+    mumbai: {
+      url: `https://rpc.ankr.com/polygon_mumbai`,
+      chainId: 80001,
+      accounts: process.env.USWAP_PRIVATE_KEY
+        ? [process.env.USWAP_PRIVATE_KEY]
+        : undefined,
+      loggingEnabled: true,
+    },
+    mantleTestnet: {
+      url: `https://rpc.testnet.mantle.xyz`,
+      chainId: 5001,
+      accounts: process.env.USWAP_PRIVATE_KEY
+        ? [process.env.USWAP_PRIVATE_KEY]
+        : undefined,
+      loggingEnabled: true,
+    },
+  },
+};
+
+export default config;
